bc3fd7c401ce03833190c8af6e302567fb1df47b
sinev-valentine <sinev-valentine@yandex.ru>
2018-11-23 22:14:44 +0300

    update RLP

--- include/eth-crypto/core/TransactionBase.h
+++ include/eth-crypto/core/TransactionBase.h
@@ -37,11 +37,11 @@ enum IncludeSignature
 	WithoutSignature = 0,	///< Do not include a signature.
 	WithSignature = 1,		///< Do include a signature.
 };
-
-/*enum class CheckTransaction
+/*
+enum struct CheckTransaction
 {
-	None,
-	Cheap,
+	None =0,
+	Cheap ,
 	Everything
 };
 */
@@ -52,10 +52,16 @@ public:
 	/// Constructs a null transaction.
 	TransactionBase() {}
 
-
+    Address const& sender() const;
 	/// Constructs a signed message-call transaction.
 	TransactionBase(u256 const& _value, u256 const& _gasPrice, u256 const& _gas, Address const& _dest, bytes const& _data, u256 const& _nonce, Secret const& _secret, int _chain_id): m_type(MessageCall), m_nonce(_nonce), m_value(_value), m_receiveAddress(_dest), m_gasPrice(_gasPrice), m_gas(_gas), m_data(_data), m_chainId(_chain_id) { sign(_secret); }
 
+  //  explicit TransactionBase(bytesConstRef _rlp, CheckTransaction _checkSig);
+  explicit TransactionBase(bytesConstRef _rlp, int _checkSig);
+
+    /// Constructs a transaction from the given RLP.
+//    explicit TransactionBase(bytes const& _rlp, CheckTransaction _checkSig): TransactionBase(&_rlp, _checkSig) {}
+    explicit TransactionBase(bytes const& _rlp, int _checkSig): TransactionBase(&_rlp, _checkSig) {}
 
 
 	/// Serialises this transaction to an RLPStream.
--- src/core/TransactionBase.cpp
+++ src/core/TransactionBase.cpp
@@ -90,11 +90,95 @@ h256 TransactionBase::sha3(IncludeSignature _sig) const
 
 	RLPStream s;
 	streamRLP(s, _sig, m_chainId > 0 && _sig == WithoutSignature);
-
 	h256 ret;
 	/*auto ret = dev::sha3(s.out());
 	if (_sig == WithSignature)
 		m_hashWith = ret;
 	 */
 	return ret;
+}
+
+
+
+//TransactionBase::TransactionBase(bytesConstRef _rlpData, CheckTransaction _checkSig)
+TransactionBase::TransactionBase(bytesConstRef _rlpData, int _checkSig)
+{
+    RLP const rlp(_rlpData);
+    try
+    {
+        if (!rlp.isList())
+            throw std::runtime_error("transaction RLP must be a list");
+
+        m_nonce = rlp[0].toInt<u256>();
+        m_gasPrice = rlp[1].toInt<u256>();
+        m_gas = rlp[2].toInt<u256>();
+        m_type = rlp[3].isEmpty() ? ContractCreation : MessageCall;
+        m_receiveAddress = rlp[3].isEmpty() ? Address() : rlp[3].toHash<Address>(RLP::VeryStrict);
+        m_value = rlp[4].toInt<u256>();
+
+        if (!rlp[5].isData())
+            throw std::runtime_error("transaction data RLP must be an array");
+
+        m_data = rlp[5].toBytes();
+
+        int const v = rlp[6].toInt<int>();
+        h256 const r = rlp[7].toInt<u256>();
+        h256 const s = rlp[8].toInt<u256>();
+
+        if (isZeroSignature(r, s))
+        {
+            m_chainId = v;
+            m_vrs = SignatureStruct{r, s, 0};
+        }
+        else
+        {
+            if (v > 36)
+                m_chainId = (v - 35) / 2;
+            else if (v == 27 || v == 28)
+                m_chainId = -4;
+            else
+                throw std::runtime_error("error");
+
+            m_vrs = SignatureStruct{r, s, static_cast<byte>(v - (m_chainId * 2 + 35))};
+
+//            if (_checkSig >= CheckTransaction::Cheap && !m_vrs->isValid())
+            if (_checkSig >= 1 && !m_vrs->isValid())
+                throw std::runtime_error("error");
+        }
+
+//        if (_checkSig == CheckTransaction::Everything)
+        if (_checkSig == 2)
+            m_sender = sender();
+
+        if (rlp.itemCount() > 9)
+            throw std::runtime_error("error");
+    }
+    catch (Exception& _e)
+    {
+        throw std::runtime_error("error");
+    }
+}
+
+#include <eth-crypto/core/sha3_wrap.h>
+
+Address const& TransactionBase::sender() const
+{
+    if (!m_sender)
+    {
+        if (hasZeroSignature())
+            m_sender = MaxAddress;
+        else
+        {
+            if (!m_vrs)
+                throw std::runtime_error("error");
+
+            auto p = recover(*m_vrs, sha3(WithoutSignature));
+            if (!p)
+                throw std::runtime_error("error");
+
+            std::vector<unsigned char> buf(p.data(), p.data()+ p.size);
+            m_sender = right160(dev::ethash::sha3_ethash(buf));
+        }
+    }
+    return m_sender;
 }
